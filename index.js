0ba2d450071dd39d4bfb500cc4ad572fe4bfca32
Dhaibuna <nataschagritte@gmail.com>
2021-04-22 10:58:48 +0200

    Update content

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-#!/usr/bin/env node 
+#!/usr/bin/env node
 
 // "Requires" for the packages 
 const chalk = require("chalk");
+const gradient = require('gradient-string');
 const boxen = require("boxen");
 
 
 // Variables
 
-const log = console.log ;
+const log = console.log;
 const newLine = "\n";
+const gold = chalk.keyword('gold');
+const paleGold = chalk.keyword('khaki');
 
 
 // Card
@@ -16,11 +19,30 @@ const newLine = "\n";
 const data = {
     name: chalk.white("Natascha Gritte"),
     alias: chalk.cyan("Nana"),
-    description: chalk.white("Studied ") + chalk.red("Chinese ") + chalk.white("Language and Literature") + '. ' + chalk.white("Currently in retraining as a ") + chalk.bold.magenta("web dev junior")
+    description: chalk.white("Studied ") + chalk.red.bold("Chinese ") + chalk.white("Language and Literature") + '. ' + chalk.white("Currently in retraining as a ") + gradient.rainbow("Web dev junior") + '.',
+    github: chalk.gray("https://github.com/Dhaibuna/ ") + paleGold("Dhaibuna"),
+    linkedIn: chalk.gray("www.linkedin.com/in/ngritte/ ") + paleGold("Natascha Gritte"),
+
+    labelGitHub: paleGold("GitHub:"),
+    labelLinkedIn: paleGold("LinkedIn:"),
 };
 
 log(
-    `${data.name} / ${data.alias} prefered`,
-    newLine, 
-    `${data.description}`, 
+    gold(
+        boxen(
+            [
+                `${data.name} / ${data.alias}`,
+                newLine,
+                `${data.description}`,
+                newLine,
+                `${data.labelGitHub} ${data.github}`,
+                `${data.labelLinkedIn} ${data.linkedIn}`
+            ].join(newLine),
+            {
+                padding: 1,
+                margin: 1,
+                borderStyle: "singleDouble",
+            },
+        )
+    )
 )
