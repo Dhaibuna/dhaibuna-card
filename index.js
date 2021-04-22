a2aae10960d0a36a8625aabf177451e5870ddd55
Dhaibuna <nataschagritte@gmail.com>
2021-04-22 11:05:41 +0200

    Update content

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const boxen = require("boxen");
 const log = console.log;
 const newLine = "\n";
 const gold = chalk.keyword('gold');
-const paleGold = chalk.keyword('khaki');
+const khaki = chalk.keyword('khaki');
 
 
 // Card
@@ -19,12 +19,12 @@ const paleGold = chalk.keyword('khaki');
 const data = {
     name: chalk.white("Natascha Gritte"),
     alias: chalk.cyan("Nana"),
-    description: chalk.white("Studied ") + chalk.red.bold("Chinese ") + chalk.white("Language and Literature") + '. ' + chalk.white("Currently in retraining as a ") + gradient.rainbow("Web dev junior") + '.',
-    github: chalk.gray("https://github.com/Dhaibuna/ ") + paleGold("Dhaibuna"),
-    linkedIn: chalk.gray("www.linkedin.com/in/ngritte/ ") + paleGold("Natascha Gritte"),
+    description: chalk.white("Studied ") + chalk.keyword("firebrick")("Chinese ") + chalk.white("Language and Literature") + '. ' + chalk.white("Currently in retraining as a ") + chalk.bold(gradient.rainbow("Web dev junior"))+ '.',
+    github: chalk.gray("https://github.com/Dhaibuna/ ") + khaki("Dhaibuna"),
+    linkedIn: chalk.gray("www.linkedin.com/in/ngritte/ ") + khaki("Natascha Gritte"),
 
-    labelGitHub: paleGold("GitHub:"),
-    labelLinkedIn: paleGold("LinkedIn:"),
+    labelGitHub: khaki("GitHub:"),
+    labelLinkedIn: khaki("LinkedIn:"),
 };
 
 log(
